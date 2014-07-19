51bdef129bba2222452a11325121800755c85dd1
Allen-yan <wentao1988@gmail.com>
2014-07-19 14:26:01 +0800

    update

--- apps/volunteer/admin.py
+++ apps/volunteer/admin.py
@@ -4,20 +4,28 @@ from apps.volunteer import models
 
 class VolunteersAdmin(admin.ModelAdmin):
     list_display = ["account", "name", "nick_name", "phone_number"]
-admin.site.register(models.Volunteers, VolunteersAdmin)
+admin.site.register(models.Volunteer, VolunteersAdmin)
 
 
 class ClassesAdmin(admin.ModelAdmin):
     list_display = ["class_name", "grade"]
-admin.site.register(models.Classes, ClassesAdmin)
+admin.site.register(models.Class, ClassesAdmin)
 
 
 class CoursesAdmin(admin.ModelAdmin):
     list_display = ["course_name"]
-admin.site.register(models.Courses, CoursesAdmin)
+admin.site.register(models.Course, CoursesAdmin)
 
+
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ["school_name", "description"]
+admin.site.register(models.School, SchoolAdmin)
 
 class ClassBeginAdmin(admin.ModelAdmin):
     list_display = ["course_id", "class_id","volunteer_id", ]
 admin.site.register(models.ClassBegin, ClassBeginAdmin)
 
+
+class CheckInAdmin(admin.ModelAdmin):
+    list_display = ["volunteer_id", "created_at"]
+admin.site.register(models.CheckIn, CheckInAdmin)
